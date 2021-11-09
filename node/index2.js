7a3411e9a8985db820fba4e45371974d93830664
Khushnood Asif <khushnood99@gmail.com>
2021-11-09 00:57:48 +0000

    combined files

--- node/index2.js
+++ node/index2.js
@@ -1,5 +1,8 @@
+const superheroes = require('superheroes');
 const supervillains = require('supervillains');
 
+const mySuperHero = superheroes.random();
 const mySuperVillan = supervillains.random();
 
+console.log(mySuperHero);
 console.log(mySuperVillan);
