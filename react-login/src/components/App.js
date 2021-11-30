cd501ecfd19252355b18dfcbed96f9c0b147f160
Khushnood Asif <khushnood99@gmail.com>
2021-11-30 00:23:27 +0000

    updated files

--- react-login/src/components/App.js
+++ react-login/src/components/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React from 'react';
 import Login from "./Login";
+import Header from './Header.js';
+import Footer from './Footer.js';
 
-var isLoggedIn = true;
+var isLoggedIn = false;
 
 const currentTime = new Date().getHours();
 console.log(currentTime);
 
 function App() {
   return (
-    <div className="container">
-      {/*Ternary Operator*/}
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+    <div>
+      <Header />
+      <div className="container">{/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello!</h1> : <Login />}
       {/*AND Operator*/}
       {currentTime > 12 && <h1>Why are you still working?</h1>}
+      </div>
+      <Footer/>
     </div>
   );
 }
--- react-login/src/index.js
+++ react-login/src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/components/App.css';
+import App from './components/App.js';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
