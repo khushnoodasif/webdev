13d7b987f6fe8799c52105e0700e9bbb7514c83c
Khushnood Asif <khushnood99@gmail.com>
2021-12-01 00:16:48 +0000

    react useState hook

---
+++ react-count/src/components/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+    <Footer /> 
+    </div>
+  );
+}
+
+export default App;
