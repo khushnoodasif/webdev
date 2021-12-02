874393a98de7f292816f766aedc09e109ded662a
Khushnood Asif <khushnood99@gmail.com>
2021-12-02 00:50:40 +0000

    react destructuring

---
+++ react-destructure/src/components/App.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import cars from './Practice.js';
+
+function App() {
+  const [honda, tesla] = cars;
+
+  const {
+    speedStats: { topSpeed: hondaTopSpeed }
+  } = honda;
+  const {
+    speedStats: { topSpeed: teslaTopSpeed }
+  } = tesla;
+  
+  const {
+    coloursByPopularity: [hondaTopColour]
+  } = honda;
+  const {
+    coloursByPopularity: [teslaTopColour]
+  } = tesla;
+
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+      </table>
+    </div>
+    <Footer /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ react-destructure/src/components/Practice.js
@@ -0,0 +1,21 @@
+const cars = [
+   {
+     model: "Honda Civic",
+     coloursByPopularity: ["Black", "Silver"],
+     speedStats: {
+       topSpeed: 140,
+       zeroToSixty: 8.5
+     }
+   },
+   {
+     model: "Tesla Model 3",
+     coloursByPopularity: ["Red", "White"],
+     speedStats: {
+       topSpeed: 150,
+       zeroToSixty: 3.2
+     }
+   }
+ ];
+ 
+ export default cars;
+ 
