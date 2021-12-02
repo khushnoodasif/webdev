734c31503d923ccb9a7040fb5c7187c4bad038ae
Khushnood Asif <khushnood99@gmail.com>
2021-12-02 01:29:33 +0000

    react forms

---
+++ react-forms/src/components/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    <Footer /> 
+    </div>
+  );
+}
+
+export default App;
