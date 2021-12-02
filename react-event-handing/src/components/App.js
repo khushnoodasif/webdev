430f83fd2bd09678fd3b791b17b9f7f65ff58e64
Khushnood Asif <khushnood99@gmail.com>
2021-12-02 01:24:48 +0000

    react event handling

---
+++ react-event-handing/src/components/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMousedOver ? "#0a192f" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+    <Footer /> 
+    </div>
+  );
+}
+
+export default App;
