17ab82c750342584e5dccde8bb00a48231c005f6
Khushnood Asif <khushnood99@gmail.com>
2021-12-07 17:46:36 +0000

    react-component-tree-v2

---
+++ react-component-tree-v2/src/components/App.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import ToDoItem from './ToDoItem.js';
+import InputArea from './InputArea.js';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+    <Footer /> 
+    </div>
+  );
+}
+
+export default App;
